d0a3a5f8d8e42175317260dd0bd6716ffebc88f4
naderalfakesh <nader.cae@gmail.com>
2019-06-01 00:25:36 +0300

    First commit

---
+++ Main.R
@@ -0,0 +1,9 @@
+# Setting the work directory
+setwd("C:/Users/nader/Desktop/kaggle")
+
+# get the data from the CSV files
+titanic.train <-  read.csv("train.csv")
+titanic.test  <-  read.csv("test.csv")
+
+head(titanic.train)
+
